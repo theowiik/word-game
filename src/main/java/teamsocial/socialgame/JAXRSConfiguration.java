d536a0dc1411ee332bf0fa14f1c3cea51fb72d5a
Theo <theo.le.wiik@gmail.com>
2021-02-02 22:45:33 +0100

    Add java-web project

---
+++ src/main/java/teamsocial/socialgame/JAXRSConfiguration.java
@@ -0,0 +1,14 @@
+package teamsocial.socialgame;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ *
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+
+}
