d1e9581fd78d5d9003b872eb6e1d30cd554e8a46
Theo <theo.le.wiik@gmail.com>
2021-02-03 10:40:36 +0100

    Add a WordDAO

--- src/main/java/teamsocial/socialgame/model/dao/AbstractDAO.java
+++ src/main/java/teamsocial/socialgame/model/dao/AbstractDAO.java
@@ -1,5 +1,26 @@
 package teamsocial.socialgame.model.dao;
 
-public abstract class AbstractDAO<T> {
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaQuery;
 
+public abstract class AbstractDAO<T> {
+  private final Class<T> entityType;
+
+  // TODO: Should this be here?
+  public AbstractDAO(Class<T> wordClass) {
+    entityType = wordClass;
+  }
+
+  protected abstract EntityManager getEntityManager();
+
+  public List<T> findAll() {
+    final CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+    cq.select(cq.from(entityType));
+    return getEntityManager().createQuery(cq).getResultList();
+  }
+
+  public void remove(T entity) {
+    getEntityManager().remove(getEntityManager().merge(entity));
+  }
 }
