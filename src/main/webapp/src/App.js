ff6c68aea4ec69ae31e25d787838b4c23b4c0f24
heinin <heinin@student.chalmers.se>
2021-02-16 15:38:17 +0100

    Removed unnecessary code and added template code in App

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Tjenis
-        </a>
-      </header>
+      <Container>
+        {" "}
+        <Row>
+          <Col>
+            <Button variant="primary">Hej</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button variant="success">hej</Button>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
