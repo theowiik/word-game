158b68a738e4506f9bc67b0d5916ee98c0e4962f
Theo <theo.le.wiik@gmail.com>
2021-03-03 12:32:27 +0100

    Add more adjectives

--- src/main/webapp/src/lib/names.js
+++ src/main/webapp/src/lib/names.js
@@ -1,6 +1,6 @@
 const names = ["Adams", "Gerdes", "Von Hacht"];
 
-const prefix = ["Supersmart", "Wise", "Cool"];
+const prefix = ["Supersmart", "Wise", "Cool", "Sofisticated", "Happy", "Intelligent"];
 
 export const getRandomName = () => {
   const randPrefix = prefix[Math.floor(Math.random() * prefix.length)];
