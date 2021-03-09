07cb109b0273e080f846cca3942a917c7d8a315a
Hentoo <heinin@student.chalmers.se>
2021-03-09 17:08:46 +0100

    Fixed tiles in voting screen.

--- frontend/src/components/PresentAnswerTile/PresentAnswerTile.jsx
+++ frontend/src/components/PresentAnswerTile/PresentAnswerTile.jsx
@@ -1,7 +1,9 @@
 export function PresentAnswerTile({ answer }) {
+  //const [isChosen, setChosen] = useState(false);
+
   return (
-    <div className="pl-2 pr-4 py-2 max-w-max bg-gray-300 flex m-4 rounded-md ">
-      <span className="text-gray-700 font-bold">"{answer}"</span>
+    <div className="pl-2 pr-4 py-2 max-w-max bg-gray-300 flex m-4 rounded-md h-16 items-center transform hover:scale-125 ">
+      <span className="text-blue-900 font-bold">"{answer}"</span>
     </div>
   );
 }
--- frontend/src/views/VotingScreen/VotingScreen.jsx
+++ frontend/src/views/VotingScreen/VotingScreen.jsx
@@ -1,16 +1,30 @@
 import { PresentAnswerTile } from "components";
 
 export function VotingScreen() {
+  const answers = [
+    { answer: "Theo e king" },
+    { answer: "Sudo e king" },
+    { answer: "Jopsidop e king" },
+  ];
+
   return (
     <div className="h-screen w-full bg-white dark:bg-gray-800 dark:text-white">
       <div className="flex flex-col align-center">
         <h1 className="text-center text-1xl font-semibold">Rappakalja</h1>
       </div>
       <div>
-        <h1 className="text-center mt-14 text-4xl font-semibold">Resonabel</h1>
+        <h1 className="text-center mt-14 text-6xl font-semibold">Resonabel</h1>
       </div>
       <div>
-        <PresentAnswerTile answer="Hej hej hej hej hej"></PresentAnswerTile>
+        <div className="flex justify-center mt-10">
+          <div className="flex flex-wrap content-start">
+            {answers.map((answer) => {
+              return (
+                <PresentAnswerTile answer={answer.answer}></PresentAnswerTile>
+              );
+            })}
+          </div>
+        </div>
       </div>
     </div>
   );
