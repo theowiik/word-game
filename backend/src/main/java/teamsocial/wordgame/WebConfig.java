cde1c16117acce0f1b80a8aa8f631dc9bbda2c28
Emanuel <isakemanuel@gmail.com>
2021-03-09 18:11:45 +0100

    Add catch-all route in Spring for react router

--- backend/src/main/java/teamsocial/wordgame/WebConfig.java
+++ backend/src/main/java/teamsocial/wordgame/WebConfig.java
@@ -3,6 +3,7 @@ package teamsocial.wordgame;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -15,6 +16,16 @@ public class WebConfig {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOriginPatterns("http://localhost:3000");
+      }
+
+      @Override
+      public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/{spring:^[a-zA-Z\\d-_]+}")
+                .setViewName("forward:/");
+        registry.addViewController("/**/{spring:^[a-zA-Z\\d-_]+}")
+                .setViewName("forward:/");
+        registry.addViewController("/{spring:^[a-zA-Z\\d-_]+}/**{spring:?!(\\.js|\\.css)$}")
+                .setViewName("forward:/");
       }
     };
   }
