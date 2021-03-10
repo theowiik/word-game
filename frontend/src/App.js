961a50dd93b0cab93344c67025c6c3a67007cdf3
Jesper <jesper@jepsoncompedit.se>
2021-03-10 10:24:23 +0100

    Added gameContext

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,23 @@ import "main.css";
 import { Route } from "react-router";
 import { BrowserRouter, Switch } from "react-router-dom";
 import { Home, Lobby, PresentWord, StartGame, VotingScreen } from "views";
+import { ManagedGameContext } from "contexts/game";
 
 function App() {
+
+
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/lobby/:pin" component={Lobby} />
-        <Route exact path="/present-word" component={PresentWord} />
-        <Route exact path="/game-settings" component={StartGame} />
-        <Route exact path="/voting-screen" component={VotingScreen} />
-      </Switch>
-    </BrowserRouter>
+    <ManagedGameContext>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/lobby/:pin" component={Lobby} />
+          <Route exact path="/present-word" component={PresentWord} />
+          <Route exact path="/game-settings" component={StartGame} />
+          <Route exact path="/voting-screen" component={VotingScreen} />
+        </Switch>
+      </BrowserRouter>
+    </ManagedGameContext>
   );
 }
 
--- frontend/src/contexts/game.js
+++ frontend/src/contexts/game.js
@@ -1,13 +1,72 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useMemo } from "react";
 const GameContext = React.createContext();
 
-export function useGameContext() {
-    return useContext(GameContext);
+const initialState = {
+  lobby: true,
+  playing: false,
+  end: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "OPEN_LOBBY": {
+      return {
+        lobby: true,
+        playing: false,
+        end: false,
+      };
+    }
+    case "START_GAME": {
+      return {
+        lobby: false,
+        playing: true,
+        end: false,
+      };
+    }
+    case "END_GAME": {
+      return {
+        lobby: false,
+        playing: false,
+        end: true,
+      };
+    }
+    default: {
+      return {
+        lobby: true,
+        playing: false,
+        end: false,
+      };
+    }
   }
-
-export function GameProvider({children}) {
-
-    const [players, setPlayers] = useState([])
-
-
 }
+export const GameProvider = (props) => {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+
+  const openLobby = () => dispatch({ type: "OPEN_LOBBY" });
+  const startGame = () => dispatch({ type: "START_GAME" });
+  const endGame = () => dispatch({ type: "END_GAME" });
+
+  const value = useMemo(
+    () => ({
+      ...state,
+      openLobby,
+      startGame,
+      endGame,
+    }),
+    [state]
+  );
+
+  return <GameContext.Provider value={value} {...props} />;
+};
+
+export const useGame = () => {
+  const context = React.useContext(GameContext);
+  if (context === undefined) {
+    throw new Error(`useGame must be used within a UIProvider`);
+  }
+  return context;
+};
+
+export const ManagedGameContext = ({ children }) => (
+  <GameProvider>{children}</GameProvider>
+);
