75b93398a9e451bbdaf03adc2f640d53dbb9fe91
Jesper <jesper@jepsoncompedit.se>
2021-03-11 08:49:45 +0100

    Removed center game content

--- frontend/src/components/GameLayout/GameLayout.jsx
+++ frontend/src/components/GameLayout/GameLayout.jsx
@@ -5,7 +5,7 @@ export const GameLayout = ({ children }) => {
   return (
     <div className=" bg-white text-gray-800 dark:text-white dark:bg-gray-800">
       <Navbar label="Rappakalja" />
-      <div className="w-full min-h-screen flex justify-center items-center">
+      <div className="w-full min-h-screen">
         {children}
       </div>
     </div>
