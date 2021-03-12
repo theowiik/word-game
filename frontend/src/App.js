9e6449f7f7a86a5cbb7a8c03fdfe2de45748680f
Jesper <jesper@jepsoncompedit.se>
2021-03-12 16:32:04 +0100

    removed all unused views

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import "App.css";
-import "main.css";
-import { Route } from "react-router";
-import { BrowserRouter, Switch } from "react-router-dom";
-import { Home, Game, Lobby, PresentWord, StartGame, VotingScreen } from "views";
-import { ManagedGameContext } from "contexts/game";
+import 'App.css';
+import 'main.css';
+import { Route } from 'react-router';
+import { BrowserRouter, Switch } from 'react-router-dom';
+import { Home, Game, StartGame } from 'views';
+import { ManagedGameContext } from 'contexts/game';
 
 function App() {
-
-
   return (
     <ManagedGameContext>
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/game/:pin" component={Game} />
-          <Route exact path="/lobby/:pin" component={Lobby} />
-          <Route exact path="/present-word" component={PresentWord} />
           <Route exact path="/game-settings" component={StartGame} />
-          <Route exact path="/voting-screen" component={VotingScreen} />
         </Switch>
       </BrowserRouter>
     </ManagedGameContext>
--- frontend/src/views/index.js
+++ frontend/src/views/index.js
@@ -1,6 +1,4 @@
 export * from "./Home";
 export * from "./Game";
-export * from "./Lobby";
-export * from "./PresentWord";
 export * from "./StartGame";
-export * from "./VotingScreen";
+
