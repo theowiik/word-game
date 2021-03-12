4c7f40e1191306addc37d1b1a1cdabe9ae336724
Jesper <jesper@jepsoncompedit.se>
2021-03-12 11:09:16 +0100

    Added ui for selecting category

--- frontend/src/views/StartGame/StartGame.jsx
+++ frontend/src/views/StartGame/StartGame.jsx
@@ -1,40 +1,53 @@
-import Axios from "axios";
-import { Navbar } from "components";
-import { useEffect, useState } from "react";
+import Axios from 'axios';
+import { Navbar, Container } from 'components';
+import { useEffect, useState } from 'react';
+import { Button } from 'components/Button/Button';
 
 export function StartGame() {
   const [categories, setCategories] = useState([]);
 
   const fetchCategories = () =>
-    Axios.get("/categories")
+    Axios.get('/categories')
       .then((res) => {
         if (Array.isArray(res.data)) setCategories(res.data);
       })
       .catch((err) => {
-        console.log("Failed to fetch categories");
+        console.log('Failed to fetch categories');
         console.log(err);
       });
+
+  const handleStartGame = () => {
+    //TODO: Post start game with new pin
+
+  };
 
   useEffect(() => {
     fetchCategories();
   }, []);
 
   return (
-    <div className="w-full h-screen bg-white dark:bg-gray-800 justify-center">
+    <div className="w-full min-h-screen  bg-white dark:text-white dark:bg-gray-800 justify-center">
       <Navbar label="Game Settings" onBackClickPath="/" />
-      
-      <div>
-        <h2 className="text-2xl font-bold text-center">Kategorier</h2>
-        <ul>
-          {categories.map((category) => (
-            <>
-              <input type="checkbox" value={category.name} />
-              <label class="ml-2">{category.name}</label>
-              <br></br>
-            </>
-          ))}
-        </ul>
-      </div>
+
+      <Container>
+        <div className='flex flex-col justify-center items-center'>
+          <div className="bg-gray-700 rounded-xl w-96 p-10 mt-20">
+            <h2 className="text-2xl font-bold">Select category</h2>
+            <ul>
+              {categories.map((category) => (
+                <>
+                  <input type="radio" value={category.name} />
+                  <label class="ml-2">{category.name}</label>
+                  <br></br>
+                </>
+              ))}
+            </ul>
+          </div>
+          <div className="mt-14">
+            <Button primary label="Start game" onClick={handleStartGame} />
+          </div>
+        </div>
+      </Container>
     </div>
   );
 }
