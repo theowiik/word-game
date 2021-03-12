c91d4e42eb4d6c06009feb0e306b443eede12b80
Jesper <jesper@jepsoncompedit.se>
2021-03-12 11:28:43 +0100

    Added post for creating new game

--- frontend/src/views/StartGame/StartGame.jsx
+++ frontend/src/views/StartGame/StartGame.jsx
@@ -16,8 +16,21 @@ export function StartGame() {
         console.log(err);
       });
 
-  const handleStartGame = () => {
-    //TODO: Post start game with new pin
+  const handleStartGame = (event) => {
+    event.preventDefault()
+    const chosenCategory = event.target.category.value
+    console.log(chosenCategory)
+    //Post start game with new pin
+    Axios.post(`/games/${chosenCategory}`, {}).then((res) => {
+      console.log("Created a new game with category " + chosenCategory)
+      console.log(res)
+    }).catch((err) => {
+      console.log('Failed to create game')
+      console.log(err)
+    })
+
+    //Redirect to lobby
+    
 
   };
 
@@ -30,23 +43,23 @@ export function StartGame() {
       <Navbar label="Game Settings" onBackClickPath="/" />
 
       <Container>
-        <div className='flex flex-col justify-center items-center'>
+        <form onSubmit={handleStartGame} className='flex flex-col justify-center items-center'>
           <div className="bg-gray-700 rounded-xl w-96 p-10 mt-20">
             <h2 className="text-2xl font-bold">Select category</h2>
-            <ul>
+            <ul className='mt-5'>
               {categories.map((category) => (
                 <>
-                  <input type="radio" value={category.name} />
-                  <label class="ml-2">{category.name}</label>
+                  <input id='category' name='category' type="radio" value={category.name} />
+                  <label className="ml-2">{category.name}</label>
                   <br></br>
                 </>
               ))}
             </ul>
           </div>
           <div className="mt-14">
-            <Button primary label="Start game" onClick={handleStartGame} />
+            <Button primary label="Start game" type="submit" />
           </div>
-        </div>
+        </form>
       </Container>
     </div>
   );
