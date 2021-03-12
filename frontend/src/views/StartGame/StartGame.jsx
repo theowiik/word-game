20150d5cf4dbca3519b8a0e0322a9f7fc0f5bbed
Jesper <jesper@jepsoncompedit.se>
2021-03-12 12:08:56 +0100

    Redirect to corresponding lobby on creation

--- frontend/src/views/StartGame/StartGame.jsx
+++ frontend/src/views/StartGame/StartGame.jsx
@@ -1,10 +1,12 @@
 import Axios from 'axios';
 import { Navbar, Container } from 'components';
 import { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { Button } from 'components/Button/Button';
 
 export function StartGame() {
   const [categories, setCategories] = useState([]);
+  const history = useHistory();
 
   const fetchCategories = () =>
     Axios.get('/categories')
@@ -17,21 +19,23 @@ export function StartGame() {
       });
 
   const handleStartGame = (event) => {
-    event.preventDefault()
-    const chosenCategory = event.target.category.value
-    console.log(chosenCategory)
+    event.preventDefault();
+    const chosenCategory = event.target.category.value;
+    console.log(chosenCategory);
     //Post start game with new pin
-    Axios.post(`/games/${chosenCategory}`, {}).then((res) => {
-      console.log("Created a new game with category " + chosenCategory)
-      console.log(res)
-    }).catch((err) => {
-      console.log('Failed to create game')
-      console.log(err)
-    })
+    Axios.post(`/games/${chosenCategory}`, {})
+      .then((res) => {
+        console.log('Created a new game with category ' + chosenCategory);
+        console.log(res);
+        //Redirect to lobby
+        history.push(`/game/${res.data.pin}`);
+      })
+      .catch((err) => {
+        console.log('Failed to create game');
+        console.log(err);
+      });
 
-    //Redirect to lobby
-    
-
+  
   };
 
   useEffect(() => {
@@ -43,13 +47,21 @@ export function StartGame() {
       <Navbar label="Game Settings" onBackClickPath="/" />
 
       <Container>
-        <form onSubmit={handleStartGame} className='flex flex-col justify-center items-center'>
+        <form
+          onSubmit={handleStartGame}
+          className="flex flex-col justify-center items-center"
+        >
           <div className="bg-gray-700 rounded-xl w-96 p-10 mt-20">
             <h2 className="text-2xl font-bold">Select category</h2>
-            <ul className='mt-5'>
+            <ul className="mt-5">
               {categories.map((category) => (
                 <>
-                  <input id='category' name='category' type="radio" value={category.name} />
+                  <input
+                    id="category"
+                    name="category"
+                    type="radio"
+                    value={category.name}
+                  />
                   <label className="ml-2">{category.name}</label>
                   <br></br>
                 </>
