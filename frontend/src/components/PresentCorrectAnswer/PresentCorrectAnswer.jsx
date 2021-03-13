8c153edeb691868da86c602ec7859d2bc3fb14ef
Jesper <jesper@jepsoncompedit.se>
2021-03-13 15:04:19 +0100

    answer->explanation

--- frontend/src/components/PresentCorrectAnswer/PresentCorrectAnswer.jsx
+++ frontend/src/components/PresentCorrectAnswer/PresentCorrectAnswer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useGame } from 'contexts/game';
 
 export const PresentCorrectAnswer = () => {
-  const { currentWord, correctAnswer } = useGame();
+  const { currentWord, correctExplanation } = useGame();
 
  
 
@@ -11,7 +11,7 @@ export const PresentCorrectAnswer = () => {
       <p className='text-yellow-400 tracking-wide uppercase' >Correct description of</p>
       <h1 className="text-6xl font-semibold">{currentWord}</h1>
       <div className="p-20 rounded-lg bg-green-500 rounded-xl mt-14 text-white">
-        <p className="text-center font-bold text-2xl">{`"${correctAnswer}"`}</p>
+        <p className="text-center font-bold text-2xl">{`"${correctExplanation}"`}</p>
       </div>
     </div>
   );
