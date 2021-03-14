96b193ba364f777ea2569e101391b5c1b5a131fc
Theo <theo.le.wiik@gmail.com>
2021-03-14 17:12:57 +0100

    Add delete route for categories

--- src/main/java/teamsocial/wordgame/controller/CategoryController.java
+++ src/main/java/teamsocial/wordgame/controller/CategoryController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import teamsocial.wordgame.model.entity.Category;
 import teamsocial.wordgame.repository.ICategoryRepository;
+import teamsocial.wordgame.repository.IWordRepository;
 
 @RestController
 @RequestMapping("/api/v1/categories")
@@ -20,6 +22,9 @@ public class CategoryController {
 
   @Autowired
   private ICategoryRepository categoryRepository;
+
+  @Autowired
+  private IWordRepository wordRepository;
 
   @GetMapping
   public List<Category> index() {
@@ -40,15 +45,35 @@ public class CategoryController {
   @PostMapping
   public ResponseEntity<Category> create(@RequestBody Category category) {
     if (category == null) {
-      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Category is in an invalid format");
+      return ResponseEntity.badRequest().build();
     }
 
     if (categoryRepository.findById(category.getName()).isPresent()) {
-      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Category already exists");
+      return ResponseEntity.badRequest().build();
     }
 
     var savedCategory = categoryRepository.save(category);
-
     return ResponseEntity.ok(savedCategory);
+  }
+
+  @DeleteMapping
+  public ResponseEntity delete(@RequestBody Category category) {
+    if (category == null) {
+      return ResponseEntity.badRequest().build();
+    }
+
+    var categoryInDatabase = categoryRepository.findById(category.getName());
+
+    if (categoryInDatabase.isEmpty()) {
+      return ResponseEntity.notFound().build();
+    }
+
+    for (var word : categoryInDatabase.get().getWords()) {
+      word.setCategory(null);
+      wordRepository.save(word);
+    }
+
+    categoryRepository.delete(categoryInDatabase.get());
+    return ResponseEntity.noContent().build();
   }
 }
