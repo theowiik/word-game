1783fcc3f9027d16a483dfccee1833a49647e830
Theo <theo.le.wiik@gmail.com>
2021-03-15 18:37:29 +0100

    Create tests for invalid category and already existing category

--- src/test/java/teamsocial/wordgame/CategoryControllerTest.java
+++ src/test/java/teamsocial/wordgame/CategoryControllerTest.java
@@ -1,12 +1,14 @@
 package teamsocial.wordgame;
 
 import java.util.UUID;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
+import org.springframework.http.HttpStatus;
 import teamsocial.wordgame.controller.CategoryController;
 import teamsocial.wordgame.model.entity.Category;
+import teamsocial.wordgame.repository.ICategoryRepository;
 
 @SpringBootTest
 class CategoryControllerTest {
@@ -14,14 +16,34 @@ class CategoryControllerTest {
   @Autowired
   private CategoryController categoryController;
 
+  @Autowired
+  private ICategoryRepository categoryRepository;
+
   @Test
-  void createTest() {
+  void createValidTest() {
     var name = getUnusedName();
     var categoryRequest = new Category(name);
     var response = categoryController.create(categoryRequest);
     var responseCategory = response.getBody();
 
-    Assert.isTrue(responseCategory.getName().equals(name));
+    Assertions.assertEquals(responseCategory.getName(), name);
+  }
+
+  @Test
+  void createInvalidTest() {
+    var response = categoryController.create(null);
+    Assertions.assertEquals(response.getStatusCode(), HttpStatus.BAD_REQUEST);
+  }
+
+  @Test
+  void createAlreadyExistingTest() {
+    var sharedName = getUnusedName();
+    var myCat = new Category(sharedName);
+    categoryRepository.save(myCat);
+
+    var response = categoryController.create(new Category(sharedName));
+
+    Assertions.assertEquals(response.getStatusCode(), HttpStatus.BAD_REQUEST);
   }
 
   private String getUnusedName() {
