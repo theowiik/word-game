98f18c3ac27de9df9cd9d6ab3eb10cd290fad7fa
Theo <theo.le.wiik@gmail.com>
2021-03-16 19:47:56 +0100

    Remove dependency from model to websocket package

--- src/main/java/teamsocial/wordgame/model/GameManagerBean.java
+++ src/main/java/teamsocial/wordgame/model/GameManagerBean.java
@@ -3,24 +3,25 @@ package teamsocial.wordgame.model;
 import java.util.HashMap;
 import java.util.Map;
 import javax.annotation.PostConstruct;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.ApplicationScope;
 import teamsocial.wordgame.model.game.Game;
+import teamsocial.wordgame.model.game.Game.GameChangedObserver;
+import teamsocial.wordgame.model.game.Game.GameFinishedObserver;
 import teamsocial.wordgame.repository.ICategoryRepository;
-import teamsocial.wordgame.websocket.GameChangedPushService;
 
 @ApplicationScope
 @Component
-public class GameManagerBean implements Game.GameFinishedListeners {
+public class GameManagerBean implements GameFinishedObserver {
 
+  @Setter
+  private GameChangedObserver pushService;
   private Map<String, Game> games;
 
   @Autowired
   private ICategoryRepository categoryRepository;
-
-  @Autowired
-  private GameChangedPushService pushService;
 
   @PostConstruct
   private void init() {
@@ -48,7 +49,11 @@ public class GameManagerBean implements Game.GameFinishedListeners {
 
     var pin = getUnusedPin();
     var game = new Game(category, pin);
-    game.addGameChangedObserver(pushService);
+
+    if (pushService != null) {
+      game.addGameChangedObserver(pushService);
+    }
+
     game.addGameFinishedListener(this);
     games.put(pin, game);
     return game;
--- src/main/java/teamsocial/wordgame/websocket/GameChangedPushService.java
+++ src/main/java/teamsocial/wordgame/websocket/GameChangedPushService.java
@@ -1,19 +1,26 @@
 package teamsocial.wordgame.websocket;
 
+import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import teamsocial.wordgame.model.GameManagerBean;
 import teamsocial.wordgame.model.game.Game;
+import teamsocial.wordgame.model.game.Game.GameChangedObserver;
 
 @Service
-public class GameChangedPushService implements Game.GameObserver {
+public class GameChangedPushService implements GameChangedObserver {
 
   @Autowired
   private SimpMessagingTemplate simpMessagingTemplate;
 
   @Autowired
   private GameManagerBean gameManager;
+
+  @PostConstruct
+  private void init() {
+    gameManager.setPushService(this);
+  }
 
   @Override
   public void notifyGameChanged(Game game) {
